99568c3afb94c63ffdf982a2651744eb542a9e32
htariku <hgtariku@yahoo.com>
2021-12-26 04:49:29 -0500

    update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,11 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "repos";
+
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+     response.json().then(function(data) {  
+  console.log(data);
+});
+});
 };
-getUserRepos();
+getUserRepos("lernantino");
