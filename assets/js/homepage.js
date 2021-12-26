a160062584929c521c64eabe7c4e907b46408837
htariku <hgtariku@yahoo.com>
2021-12-26 13:48:45 -0500

    buttons and event listeners

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,11 @@
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var formSubmitHandler = function (event) {
+var formSubmitHandler = function(event) {
+
   event.preventDefault();
 
   var username = nameInputEl.value.trim();
@@ -11,6 +13,7 @@ var formSubmitHandler = function (event) {
   if (username) {
     getUserRepos(username);
 
+  
     repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
@@ -18,14 +21,27 @@ var formSubmitHandler = function (event) {
   }
 };
 
-var getUserRepos = function (user) {
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = "";
+  }
+};
+
+var getUserRepos = function(user) {
+  
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+
   fetch(apiUrl)
-    .then(function (response) {
+    .then(function(response) {
+ 
       if (response.ok) {
         console.log(response);
-        response.json().then(function (data) {
+        response.json().then(function(data) {
           console.log(data);
           displayRepos(data, user);
         });
@@ -33,12 +49,30 @@ var getUserRepos = function (user) {
         alert("Error: " + response.statusText);
       }
     })
-    .catch(function (error) {
+    .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
 };
 
-var displayRepos = function (repos, searchTerm) {
+var getFeaturedRepos = function(language) {
+  
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+
+  fetch(apiUrl).then(function(response) {
+ 
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+var displayRepos = function(repos, searchTerm) {
+
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
@@ -47,34 +81,40 @@ var displayRepos = function (repos, searchTerm) {
   repoSearchTerm.textContent = searchTerm;
 
   for (var i = 0; i < repos.length; i++) {
+
     var repoName = repos[i].owner.login + "/" + repos[i].name;
+
 
     var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
     repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
+  
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
 
+ 
     repoEl.appendChild(titleEl);
 
+   
     var statusEl = document.createElement("span");
     statusEl.classList = "flex-row align-center";
 
+  
     if (repos[i].open_issues_count > 0) {
       statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" +
-        repos[i].open_issues_count +
-        " issue(s)";
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
     } else {
-      statusEl.innerHTML =
-        "<i class='fas fa-check-square status-icon icon-success'></i>";
+      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
+
     repoEl.appendChild(statusEl);
+
 
     repoContainerEl.appendChild(repoEl);
   }
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
