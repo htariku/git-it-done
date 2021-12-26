2b29467a7ac5ae67dc371e66cd44fba810cc9b80
htariku <hgtariku@yahoo.com>
2021-12-26 13:06:08 -0500

    query sring amd parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,19 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -15,8 +28,7 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -63,4 +75,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
